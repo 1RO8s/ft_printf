383764d6aa192a2bbb0b6f4ba8aabd6eb694fa95
hnagasak <hnagasak@student.42tokyo.jp>
2023-06-25 18:44:06 +0900

    include libft.h

--- ft_printf.h
+++ ft_printf.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 16:28:46 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/06/25 16:35:40 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/06/25 17:45:52 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
+#include "./libft/libft.h"
 
 # include <stdarg.h>
 # include <stdio.h>
