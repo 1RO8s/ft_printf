8c4eaee5a3506493052f1ed3d8081d53534b91dd
hnagasak <hnagasak@student.42tokyo.jp>
2023-07-06 07:51:48 +0900

    add processing for %u and %X

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,35 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 14:54:23 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/07/06 05:37:43 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/07/06 07:48:53 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// static int	count_hex_digit(int num)
-// {
-// 	int	digit;
-// 	int	n;
+static int	ft_count_hex_digit(unsigned int num)
+{
+	int				digit;
+	unsigned int	n;
 
-// 	digit = 1;
-// 	n = num / 16;
-// 	while (n != 0 && digit++)
-// 		n = n / 16;
-// 	return (digit);
-// }
+	digit = 1;
+	n = num / 16;
+	while (n != 0 && digit++)
+		n = n / 16;
+	return (digit);
+}
 
-// char	*toHex(int num)
-// {
-// 	char	*hex;
+static int	ft_count_uint_digit(unsigned int num)
+{
+	int				digit;
+	unsigned int	n;
 
-// 	if (0 <= num && num < 10)
-// 		*hex = '0' + num;
-// 	return (char *)('0' + num);
-// }
+	digit = 1;
+	n = num / 10;
+	while (n != 0 && digit++)
+		n = n / 10;
+	return (digit);
+}
 
 int	ft_putnbr(int num)
 {
@@ -63,6 +66,99 @@ int	ft_putchar(char c)
 	return (1);
 }
 
+void	ft_ui2a(char *str, unsigned int n, int len)
+{
+	if (0 <= n && n < 10)
+		*str = '0' + n;
+	else if (10 <= n)
+	{
+		ft_ui2a(str, n / 10, len - 1);
+		*(str + len - 1) = '0' + (n % 10);
+	}
+}
+
+int	ft_putuint(unsigned int num)
+{
+	char	*result;
+	int		len;
+
+	len = ft_count_uint_digit(num);
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (-1);
+	ft_ui2a(result, num, len);
+	ft_putstr(result);
+	free(result);
+	return (len);
+}
+
+int	ft_putptr(uintptr_t ptr)
+{
+	printf("printf pointer %p", (void *)ptr);
+	return (1);
+}
+
+char	ft_get_hex_base(unsigned int n)
+{
+	if (0 <= n && n < 9)
+		return ('0' + n);
+	else if (10 <= n && n < 16)
+		return ('A' + (n - 10));
+	else
+		return (0);
+}
+
+char	*ft_get_hex_str(char *str, unsigned int n, int len)
+{
+	if (0 <= n && n < 16)
+		*str = ft_get_hex_base(n);
+	else if (16 <= n)
+	{
+		ft_get_hex_str(str, n / 16, len - 1);
+		*(str + len - 1) = ft_get_hex_base(n % 16);
+	}
+	return (str);
+}
+
+int	ft_puthex(unsigned int num)
+{
+	char	*str;
+	int		len;
+
+	len = ft_count_hex_digit(num);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (-1);
+	str = ft_get_hex_str(str, num, len);
+	ft_putstr(str);
+	free(str);
+	return (len);
+}
+
+int	ft_print_specifier(char *c, va_list args)
+{
+	int	length;
+
+	length = 0;
+	if (*c == 'c')
+		length = ft_putchar((char)va_arg(args, int));
+	else if (*c == 's')
+		length = ft_putstr(va_arg(args, char *));
+	else if (*c == 'p')
+		length = ft_putptr(va_arg(args, uintptr_t));
+	else if (*c == 'd' || *c == 'i')
+		length = ft_putnbr(va_arg(args, int));
+	else if (*c == 'u')
+		length = ft_putuint(va_arg(args, unsigned int));
+	else if (*c == 'x' || *c == 'X')
+		length = ft_puthex(va_arg(args, unsigned int));
+	else if (*c == '%')
+		length = ft_putchar('%');
+	else
+		length = ft_putchar(*c);
+	return (length);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
@@ -75,29 +171,10 @@ int	ft_printf(const char *format, ...)
 	while (*c != '\0')
 	{
 		if (*c == '%')
-		{
-			c++;
-			if (*c == 'c')
-				length += ft_putchar((char)va_arg(args, int));
-			else if (*c == 's')
-				length += ft_putstr(va_arg(args, char *));
-			else if (*c == 'p')
-				printf("printf pointer");
-			else if (*c == 'd' || *c == 'i')
-				length += ft_putnbr(va_arg(args, int));
-			else if (*c == 'u')
-				printf("printf unsinged int");
-			else if (*c == 'x' || *c == 'X')
-				printf("printf lower hexadecimal");
-			else if (*c == '%')
-				length += ft_putchar('%');
-			else
-				ft_putchar_fd(*c, 1);
-		}
+			length += ft_print_specifier(++c, args);
 		else
 			length += ft_putchar(*c);
 		c++;
-		// length++;
 	}
 	return (length);
 }
