f55cff662266872464caafb4d1d9ec7fa877c898
hnagasak <hnagasak@student.42tokyo.jp>
2023-07-06 08:31:16 +0900

    fix header file

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,18 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 16:28:46 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/06/25 17:45:52 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/07/06 08:30:10 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "./libft/libft.h"
-
+# include "./libft/libft.h"
+# include <limits.h>
 # include <stdarg.h>
 # include <stdio.h>
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
