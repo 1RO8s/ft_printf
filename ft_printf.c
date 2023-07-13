65e86ba64dad9fb0698ef368fe64b3e86f0f2616
hnagasak <hnagasak@student.42tokyo.jp>
2023-07-14 04:41:38 +0900

    create approximate function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 14:54:23 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/07/06 07:48:53 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/07/14 04:37:39 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,18 @@ static int	ft_count_uint_digit(unsigned int num)
 	return (digit);
 }
 
+static int	ft_count_ptr_digit(uintptr_t num)
+{
+	int			digit;
+	uintptr_t	n;
+
+	digit = 1;
+	n = num / 16;
+	while (n != 0 && digit++)
+		n = n / 16;
+	return (digit);
+}
+
 int	ft_putnbr(int num)
 {
 	int	len;
@@ -62,8 +74,23 @@ int	ft_putstr(char *str)
 
 int	ft_putchar(char c)
 {
-	ft_putchar_fd(c, 1);
-	return (1);
+	int	len;
+
+	len = 1;
+	// printf("(c:%c) ",c);
+	// if (c == '0'){
+	// printf("null char\n");
+	// 	// len = ft_putstr("^@");
+	// }
+	// else
+	if (c == 0)
+	{
+		printf("null");
+		return (0);
+	}
+	else
+		ft_putchar_fd(c, 1);
+	return (len);
 }
 
 void	ft_ui2a(char *str, unsigned int n, int len)
@@ -92,35 +119,31 @@ int	ft_putuint(unsigned int num)
 	return (len);
 }
 
-int	ft_putptr(uintptr_t ptr)
-{
-	printf("printf pointer %p", (void *)ptr);
-	return (1);
-}
-
-char	ft_get_hex_base(unsigned int n)
+char	ft_get_hex_base(unsigned int n, int isUpper)
 {
 	if (0 <= n && n < 9)
 		return ('0' + n);
-	else if (10 <= n && n < 16)
+	else if (10 <= n && n < 16 && isUpper)
 		return ('A' + (n - 10));
+	else if (10 <= n && n < 16 && !isUpper)
+		return ('a' + (n - 10));
 	else
 		return (0);
 }
 
-char	*ft_get_hex_str(char *str, unsigned int n, int len)
+char	*ft_get_hex_str(char *str, unsigned int n, int len, int isUpper)
 {
 	if (0 <= n && n < 16)
-		*str = ft_get_hex_base(n);
+		*str = ft_get_hex_base(n, isUpper);
 	else if (16 <= n)
 	{
-		ft_get_hex_str(str, n / 16, len - 1);
-		*(str + len - 1) = ft_get_hex_base(n % 16);
+		ft_get_hex_str(str, n / 16, len - 1, isUpper);
+		*(str + len - 1) = ft_get_hex_base(n % 16, isUpper);
 	}
 	return (str);
 }
 
-int	ft_puthex(unsigned int num)
+int	ft_puthex(unsigned int num, int isUpper)
 {
 	char	*str;
 	int		len;
@@ -129,10 +152,38 @@ int	ft_puthex(unsigned int num)
 	str = (char *)malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (-1);
-	str = ft_get_hex_str(str, num, len);
+	str = ft_get_hex_str(str, num, len, isUpper);
 	ft_putstr(str);
 	free(str);
 	return (len);
+}
+
+char	*ft_get_ptr_str(char *str, uintptr_t n, int len)
+{
+	if (0 <= n && n < 16)
+		*str = ft_get_hex_base(n, 0);
+	else if (16 <= n)
+	{
+		ft_get_ptr_str(str, n / 16, len - 1);
+		*(str + len - 1) = ft_get_hex_base(n % 16, 0);
+	}
+	return (str);
+}
+
+int	ft_putptr(uintptr_t ptr)
+{
+	char	*str;
+	int		len;
+
+	len = ft_count_ptr_digit(ptr);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (-1);
+	str = ft_get_ptr_str(str, ptr, len);
+	ft_putstr("0x");
+	ft_putstr(str);
+	free(str);
+	return (len + 2);
 }
 
 int	ft_print_specifier(char *c, va_list args)
@@ -150,8 +201,10 @@ int	ft_print_specifier(char *c, va_list args)
 		length = ft_putnbr(va_arg(args, int));
 	else if (*c == 'u')
 		length = ft_putuint(va_arg(args, unsigned int));
-	else if (*c == 'x' || *c == 'X')
-		length = ft_puthex(va_arg(args, unsigned int));
+	else if (*c == 'X')
+		length = ft_puthex(va_arg(args, unsigned int), 1);
+	else if (*c == 'x')
+		length = ft_puthex(va_arg(args, unsigned int), 0);
 	else if (*c == '%')
 		length = ft_putchar('%');
 	else
