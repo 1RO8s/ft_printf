3c79b179a1f5ba247912e4f2a9aa7b09d4350d35
hnagasak <hnagasak@student.42tokyo.jp>
2023-07-14 08:57:34 +0900

    add process to free after ft_itoa

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 04:05:08 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/19 04:07:55 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/07/14 08:54:47 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char *str = ft_itoa(n);
+	ft_putstr_fd(str, fd);
+	free(str);
 }
